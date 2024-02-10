fabe18244a758278e27ed33e46c54f0985a8b588
SriCoD <72248660+SriCoD@users.noreply.github.com>
2024-02-11 02:09:39 +0530

    changes

--- src/components/Checkout/BottomFoot.jsx
+++ src/components/Checkout/BottomFoot.jsx
@@ -1,51 +1,34 @@
-// import React from "react";
+import React from "react";
 
-// const BottomFoot = () => {
-//   return (
-//     <div className="bottom_foot">
-//       <div>
-//         <img
-//           src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_284/Logo_f5xzza"
-//           alt=""
-//         />
-//       </div>
-//       <div style={{ color: "white" }}></div>
-//       <div>
-//         <div>
-//           <a href="">
-//             <img
-//               src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-facebook_tfqsuc"
-//               alt=""
-//             />
-//           </a>
-//         </div>
-//         <div>
-//           <a href="">
-//             <img
-//               src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-pinterest_kmz2wd"
-//               alt=""
-//             />
-//           </a>
-//         </div>
-//         <div>
-//           <a href="">
-//             <img
-//               src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-instagram_b7nubh"
-//               alt=""
-//             />
-//           </a>
-//         </div>
-//         <div>
-//           <a href="">
-//             <img
-//               src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-twitter_gtq8dv"
-//               alt=""
-//             />
-//           </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const BottomFoot = () => {
+  return (
+    <div className="bottom_foot">
+      <div>
+        <a href="">
+          <img
+            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-pinterest_kmz2wd"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-instagram_b7nubh"
+            alt=""
+          />
+        </a>
+      </div>
+      <div>
+        <a href="">
+          <img
+            src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_48,h_48/icon-twitter_gtq8dv"
+            alt=""
+          />
+        </a>
+      </div>
+    </div>
+  );
+};
 
-// export default BottomFoot;
+export default BottomFoot;
